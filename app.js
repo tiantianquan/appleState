627ed439ca521060fc5e72e1caf979cf26f13950
Vegeta <576826394@qq.com>
2016-10-08 17:09:41 +0800

    1

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app)
-var fs = require('fs')
+var koa = require('koa')
+var router = require('koa-router')()
+var send = require('koa-send')
 
-app.listen(9999)
+var app = koa()
+router.get('/index.html',function* (next) {
+    yield send(this,'./index.html')
+})
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500)
-      return res.end('Error loading index.html')
-    }
+app.use(router.routes())
+ .use(router.allowedMethods())
 
-    res.writeHead(200)
-    res.end(data)
-  })
-}
+var server = require('http').Server(app.callback())
+var     io = require('socket.io')(server)
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' })
-  socket.on('my other event', function (data) {
-    console.log(data)
-  })
+    socket.emit('news', { hello: 'world' })
+    socket.on('my other event', function (data) {
+        console.log(data)
+    })
 })
+server.listen(9999)
